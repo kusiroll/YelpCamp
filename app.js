109f32eb88bc2b9ed094455d0f17c4cdc5ee46eb
Ozioma Nsaka <kusiroll@yahoo.com>
2019-07-16 04:09:03 -0500

    add server listen

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen(3000, function(){
-    console.log("The YelpCamp Server Has Started");
-});
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log('Server listening on port: ' + process.env.PORT);
+  });
