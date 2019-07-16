d198e9bd887c5c44b60b0932b81528293ab17fd8
Ozioma Nsaka <kusiroll@yahoo.com>
2019-07-16 04:06:41 -0500

    mongo lab

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var express     = require("express"),
 var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes     = require("./routes/index")
-
-mongoose.connect("mongodb://localhost/yelp_camp",{ useNewUrlParser: true });
+mongoose.connect("mongodb+srv://ojnsaka:Ozzy@mwd100@cluster0-mbsgi.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true });;
+//mongoose.connect("mongodb://localhost/yelp_camp",{ useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
